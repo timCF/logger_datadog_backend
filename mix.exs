d44d355e56c9f9554fe68f989330844e19a91695
Matteo Giachino <matteog@gmail.com>
2017-02-10 22:52:57 +0100

    extras

--- mix.exs
+++ mix.exs
@@ -1,9 +1,11 @@
 defmodule LoggerDatadogBackend.Mixfile do
   use Mix.Project
 
+  @version "0.1.1"
+
   def project do
     [app: :logger_datadog_backend,
-     version: "0.1.1",
+     version: @version,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -13,22 +15,10 @@ defmodule LoggerDatadogBackend.Mixfile do
      docs: docs()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :ex_statsd]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:ex_statsd, ">= 0.5.1"},
      {:ex_doc, ">= 0.0.0", only: :dev}]
@@ -45,7 +35,8 @@ defmodule LoggerDatadogBackend.Mixfile do
   defp docs do
     [
       source_url: "https://github.com/matteosister/logger_datadog_backend",
-      extras: ["README.md"]
+      source_ref: "v#{@version}",
+      extras: ["README.md"], main: "readme"
     ]
   end
 end
